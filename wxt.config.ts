d1dcd14509caeca744384661f3ad468c650039b5
Totti Rdz <thorsten_herfurtner@gmx.de>
2026-03-25 05:39:14 +0100

    feat: always open bahn.de in development

--- wxt.config.ts
+++ wxt.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from 'wxt';
 
 export default defineConfig({
   modules: ['@wxt-dev/module-react'],
+  runner: {
+    startUrls: ['https://www.bahn.de'],
+  },
   manifest: {
     name: 'Deutsche Bahn Booking Helper',
     description:
